61b3d620c00b6d3f0055f4d7287769b2a7e2eaf3
Jason Waller <jwaller8642@mail.kvcc.edu>
2015-09-17 14:46:34 -0400

    Adds CSVProcessor and WineItemCollection

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -8,8 +8,32 @@ namespace assignment1
 {
     class Program
     {
+
+        static string UserInputMenu = string.Empty;
         static void Main(string[] args)
         {
+            Console.WriteLine ("To Veiw Wine List type list");
+            Console.WriteLine("To Search Wine Search type search");
+            Console.WriteLine("To Add New Wine type add ");
+            Console.Write("Choose an option: ");
+            UserInputMenu = Console.ReadLine();
+            Console.WriteLine("");
+            
+
+            if (UserInputMenu == "list")
+            {
+                Console.Write("You typed ih list");
+                CSVProcessor listWine = new CSVProcessor();
+            }
+            else if (UserInputMenu == "search")
+            {
+                Console.Write("You typed in search");
+            }
+            else if (UserInputMenu == "add")
+            {
+                Console.Write("You typed in add");
+            }
+            Console.ReadKey();
         }
     }
 }
